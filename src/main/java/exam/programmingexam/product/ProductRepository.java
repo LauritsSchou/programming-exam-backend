a117e4c8460307f875a4c9b82f0c986858aa76ea
LauritsSchou <laurits.schou@gmail.com>
2024-06-19 14:25:55 +0200

    created a product entity with controller and service

---
+++ src/main/java/exam/programmingexam/product/ProductRepository.java
@@ -0,0 +1,6 @@
+package exam.programmingexam.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
