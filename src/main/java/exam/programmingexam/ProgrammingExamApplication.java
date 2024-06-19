f4738d86ddb08b39409c1cfa4e9fa1a648ed0bb8
LauritsSchou <laurits.schou@gmail.com>
2024-06-19 14:11:23 +0200

    initial commit

---
+++ src/main/java/exam/programmingexam/ProgrammingExamApplication.java
@@ -0,0 +1,13 @@
+package exam.programmingexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammingExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/exam/programmingexam/ProgrammingExamApplicationTests.java
@@ -0,0 +1,13 @@
+package exam.programmingexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
